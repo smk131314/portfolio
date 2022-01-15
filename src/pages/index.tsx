cb109067784f03e04a5433b07b2fd1f9bceaa8c7
Jade.Kim <jade.kim@travelflan.com>
2022-01-15 16:30:47 +0900

    feat: add viewport meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ const Main: NextPage = () => {
       <Head>
         <title>Seungmin Kim Portfolio</title>
         <meta name="description" content="Seungmin Kim portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
