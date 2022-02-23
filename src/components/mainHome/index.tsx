358394bb53234703b509423bb7f4c472581f5cb1
Jade.Kim <jade.kim@travelflan.com>
2022-02-23 13:46:17 +0000

    feat: modify main summary text

--- src/components/mainHome/index.tsx
+++ src/components/mainHome/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { LINKS } from '@src/constants/links'
 import classNames from 'classNames/bind'
 import styles from '@styles/MainHome.module.scss'
-import { MAIN_CANVAS_ID } from '@src/constants'
+import {
+  MAIN_CANVAS_ID,
+  MAIN_SUMMARY_LINE_1,
+  MAIN_SUMMARY_LINE_2,
+} from '@src/constants'
 import { useNav } from '@src/hooks/useNav'
 import MainIconsAnimation from './MainIconsAnimation'
 import ScrollDownButton from './ScrollDownButton'
@@ -16,9 +20,13 @@ const MainHome = () => {
     <section id={LINKS.MAIN.id} className={cx('mainHome')} ref={homeRef}>
       <div className={cx('descriptionContainer')}>
         <h1>
-          Seungmin Kim. <br />a frontend developer.
+          Seungmin Kim.
+          <p>Front End Developer.</p>
         </h1>
-        <h2>with experience in Architecture</h2>
+        <h2>
+          <p>{MAIN_SUMMARY_LINE_1}</p>
+          <p>{MAIN_SUMMARY_LINE_2}</p>
+        </h2>
       </div>
       <div className={cx('canvasContainer')} id={MAIN_CANVAS_ID}>
         <MainIconsAnimation />
